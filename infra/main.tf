2b4cf921ae013627306e9338c970bdf07526c796
SATISH KUMAR REDDY ALAVALA <41616608+sruthishtechnologies@users.noreply.github.com>
2026-06-12 08:36:55 +0530

    commented the resource after testing infracost
    after verification disabled resource to avoid deployment cost

--- infra/main.tf
+++ infra/main.tf
@@ -14,18 +14,19 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "demo" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  root_block_device {
-    volume_size = var.root_volume_size
-    volume_type = "gp3"
-  }
-
-  tags = {
-    Name        = "infracost-demo"
-    Environment = "dev"
-    CostCenter  = "sre-training"
-  }
-}
+# EC2 instance creation
+#resource "aws_instance" "demo" {
+#  ami           = var.ami_id
+#  instance_type = var.instance_type
+#
+#  root_block_device {
+#    volume_size = var.root_volume_size
+#    volume_type = "gp3"
+#  }
+#
+#  tags = {
+#    Name        = "infracost-demo"
+#    Environment = "dev"
+#    CostCenter  = "sre-training"
+#  }
+#}
